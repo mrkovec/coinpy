13c6aad3a021e689f3b55b301f09aab0f6dc4894
mrkovec <mrkovec@gmail.com>
2017-11-22 16:09:34 +0100

    travis

--- tests/setup.py
+++ tests/setup.py
@@ -1,10 +1,9 @@
 import sys
 import os
 
-# PACKAGE_PARENT = os.path.join('..','..')
-# print(PACKAGE_PARENT)
-# SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
-# sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+PACKAGE_PARENT = os.path.join('..','..')
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
 from coinpy.core.crypto import Pubkey
 
